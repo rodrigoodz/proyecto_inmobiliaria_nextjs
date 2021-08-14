78da673aed59a34dec887621b2ccdc7164e443df
rodrigoodz <rodrigoodz@gmail.com>
2021-08-14 20:17:30 -0300

    agregado boton de whatsapp en modal

--- components/EstateModal.js
+++ components/EstateModal.js
@@ -8,9 +8,13 @@ import {
   ModalCloseButton,
   Text,
   Button,
+  Icon,
+  Link,
 } from "@chakra-ui/react";
+import { ImWhatsapp } from "react-icons/im";
 
 const EstateModal = ({ isOpen, onClose, estate }) => {
+  console.log(estate);
   return (
     <Modal
       isOpen={isOpen}
@@ -25,10 +29,29 @@ const EstateModal = ({ isOpen, onClose, estate }) => {
         <ModalCloseButton />
         <ModalBody>
           <Text>qweqwwqewqewqe</Text>
+          <Link
+            href={`https://api.whatsapp.com/send?phone=543456450230&text=${encodeURIComponent(
+              `Hola Melina, estoy interesado/a en la propiedad ID=${estate.id} ubicada en ${estate.domicilio}`
+            )}`}
+            isExternal
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            <Button colorScheme="green">
+              <Icon as={ImWhatsapp} />
+              <Text mt={1} ml={2} fontWeight="light">
+                Consulta esta propiedad por Whatsapp
+              </Text>
+            </Button>
+          </Link>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="purple" mr={3} onClick={onClose}>
+          <Button colorScheme="purple" onClick={onClose}>
             Cerrar
           </Button>
         </ModalFooter>
