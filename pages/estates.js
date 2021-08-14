dfdaa3b4440483548a66b4d4de98c66b81619904
rodrigoodz <rodrigoodz@gmail.com>
2021-08-14 13:03:03 -0300

    agregada logica para 'traer' la info de las propiedades

--- pages/estates.js
+++ pages/estates.js
@@ -1,8 +1,9 @@
 import { Box, Text } from "@chakra-ui/react";
 import ContactInfo from "../components/ContactInfo";
 import Navbar from "../components/Navbar";
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
 
-export default function Estates() {
+export default function Estates({ data }) {
   return (
     <Box backgroundColor="primary" minHeight="100vh" textAlign="center">
       <ContactInfo />
@@ -10,6 +11,17 @@ export default function Estates() {
       <Box maxW="5xl" margin="auto" p={5}>
         <Text>propiedades</Text>
       </Box>
+      {JSON.stringify(data, null, 2)}
     </Box>
   );
+}
+
+export async function getStaticProps() {
+  const parser = new PublicGoogleSheetsParser(process.env.SHEET_ID);
+  const data = await parser.parse();
+
+  return {
+    revalidate: 60,
+    props: { data },
+  };
 }
