797d505f4e062bd1523cf16ebfaa85beec3d403a
rodrigoodz <rodrigoodz@gmail.com>
2021-08-16 19:23:13 -0300

    añadido filtro de propiedades en estates.js

--- pages/estates.js
+++ pages/estates.js
@@ -1,4 +1,10 @@
-import { Box, SimpleGrid, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  SimpleGrid,
+  useDisclosure,
+  Select,
+  Heading,
+} from "@chakra-ui/react";
 import ContactInfo from "../components/ContactInfo";
 import Navbar from "../components/Navbar";
 import PublicGoogleSheetsParser from "public-google-sheets-parser";
@@ -10,10 +16,22 @@ import { useState } from "react";
 export default function Estates({ estates }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selected, setSelected] = useState({});
+  const [filteredEstates, setFilteredEstates] = useState(estates);
 
-  const handleSelected = (estate) => {
+  const handleClick = (estate) => {
     setSelected(estate);
     onOpen();
+  };
+
+  const handleSelect = (e) => {
+    if (e.target.value !== "") {
+      const filtered = estates.filter(
+        (estate) => e.target.value === estate.tipo
+      );
+      setFilteredEstates(filtered);
+    } else {
+      setFilteredEstates(estates);
+    }
   };
 
   return (
@@ -27,23 +45,45 @@ export default function Estates({ estates }) {
       <Navbar />
       <Box maxW="5xl" margin="auto">
         <Title text="Propiedades" />
-        {/* TODO agregar filtro para seleccionar por Casas, Locales, etc... */}
-        <SimpleGrid columns={[1, 2, 3, 3]} spacing={5} px={50} pb={10}>
-          {estates.map((estate) => {
-            return (
-              <Box onClick={() => handleSelected(estate)} key={estate.id}>
-                <Card
-                  title={estate.tipo.substr(0, estate.tipo.length - 1)}
-                  price={estate.precio}
-                  adress={estate.domicilio}
-                  currency={estate.moneda}
-                  image={estate.img1 === "null" ? null : estate.img1}
-                  description={estate.descripcion}
-                />
-              </Box>
-            );
-          })}
-        </SimpleGrid>
+        <Select
+          onChange={handleSelect}
+          variant="filled"
+          mb={4}
+          w={["xs", "sm", "xl", "2xl"]}
+          mx="auto"
+        >
+          <option value="">Todas</option>
+          <option value="CASAS">Casas</option>
+          <option value="COCHERAS">Cocheras</option>
+          <option value="DEPARTAMENTOS">Departamentos</option>
+          <option value="GALPONES">Galpones</option>
+          <option value="LOCALES">Locales</option>
+          <option value="QUINTAS">Quintas</option>
+          <option value="TERRENOS">Terrenos</option>
+          <option value="OTROS">Otros</option>
+        </Select>
+        {Boolean(filteredEstates.length > 0) ? (
+          <SimpleGrid columns={[1, 2, 3, 3]} spacing={5} px={50} pb={10}>
+            {filteredEstates.map((estate) => {
+              return (
+                <Box onClick={() => handleClick(estate)} key={estate.id}>
+                  <Card
+                    title={estate.tipo.substr(0, estate.tipo.length - 1)}
+                    price={estate.precio}
+                    adress={estate.domicilio}
+                    currency={estate.moneda}
+                    image={estate.img1 === "null" ? null : estate.img1}
+                    description={estate.descripcion}
+                  />
+                </Box>
+              );
+            })}
+          </SimpleGrid>
+        ) : (
+          <Heading as="h2" size="lg">
+            No hay propiedades de ese tipo disponibles actualmente
+          </Heading>
+        )}
       </Box>
       {selected && (
         <EstateModal isOpen={isOpen} onClose={onClose} estate={selected} />
